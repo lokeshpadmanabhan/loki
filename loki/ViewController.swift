5a2db5be20dce136fe9e01866e14b3e6f87cb95e
lokesh <lokespadmanabhan95@gmail.com>
2024-03-07 12:18:16 +0530

    Initial Commit

---
+++ loki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loki
+//
+//  Created by Lokesh Cheenu on 07/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
